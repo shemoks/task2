7d28b6d6f5e776b7f7516c58d96781e27221a4dd
shemoks <shemshur@ukr.net>
2015-10-25 13:57:50 +0200

    updade basic class, json  and create extends class

---
+++ app/Product/Product.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oksana
+ * Date: 23.10.15
+ * Time: 23:01
+ */
+
+namespace Product;
+
+
+abstract class Product
+{
+    public abstract function getName();
+
+    public abstract function setName($name);
+
+    public abstract function getCategory();
+
+    public abstract function setCategory($category);
+
+    public abstract function getPrice();
+
+    public abstract function setPrice($price);
+
+    public abstract function printProperties();
+    public abstract function discount();
+
+    public function __set($property, $value)
+    {
+        $this->$property = $value;
+    }
+
+    public function __get($property)
+    {
+        return $this->$property;
+    }
+
+}
+
--- index.php
+++ index.php
@@ -1,2 +1,43 @@
+
 <?php
 require_once "vendor/autoload.php";
+
+use Product\Book;
+
+echo "Книги<br>";
+$history = new Book();
+$history->setName("Історія України");
+$history->setCategory(1);
+$history->setAuthor("Грушевський");
+$history->setPrice(546);
+$history->setAvailability(false);
+$properties = $history->printProperties();
+echo $properties;
+$category=$history->getCategory();
+echo $category. "<br>";
+echo "Ціна зі знижкою:";
+$discount=$history->discount();
+echo "$discount <br>\n";
+echo "<br>";
+$history = new Book();
+$history->setName("Php.Обучение на примерах");
+$history->setCategory(0);
+$history->setAuthor("А.Кухарчик");
+$history->setPrice(700);
+$history->setAvailability(true);
+$properties = $history->printProperties();
+echo $properties;
+$category=$history->getCategory();
+echo $category. "<br>";
+echo "Ціна зі знижкою:";
+$discount=$history->discount();
+echo "$discount <br>\n";
+echo "<br\n";
+
+
+
+
+
+
+
+
